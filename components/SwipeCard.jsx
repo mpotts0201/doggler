d62ab0e24c557baac8e6bd2e08c9116b44accada
Murphy Potts <murphy@heliumservices.com>
2021-06-18 20:31:02 -0400

    Added rotation and scale to animation for swiping a card

--- components/SwipeCard.jsx
+++ components/SwipeCard.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import styles from "../styles/SwipeCard.module.css";
-import {animated, useSpring} from "react-spring";
+import {animated, useSpring, interpolate} from "react-spring";
 import {useDrag} from "@use-gesture/react";
 
 export default function SwipeCard(props) {
-    const [{x}, api] = useSpring(() => ({x: 0}));
+    const [{x, y, rotateZ, scale}, api] = useSpring(() => ({x: 0, y: 0, rotateZ: 0, scale: 1}));
 
-    const bind = useDrag(({down, movement: [mx], velocity, direction: [xDir]}) => {
+    const bind = useDrag(({down, movement: [mx, my], velocity, direction: [xDir]}) => {
         let deltaX = 0;
+        // let deltaY = 0;
         const dir = xDir > 0 ? 1 : -1;
+        const rotation = mx / 100;
         if (!down && velocity[0] > 0.2) {
             deltaX = (200 + window.innerWidth) * dir;
+            // deltaY = 200 + window.innerHeight;
         }
 
-        api.start({x: down ? mx : deltaX});
+        api.start({x: down ? mx : deltaX, rotateZ: down ? rotation : 0, scale: down ? 1.1 : 1});
     });
     return (
-        <animated.div {...bind()} style={{x, touchAction: "none"}} className={styles.card}>
+        <animated.div {...bind()} style={{x, touchAction: "none", rotateZ, scale}} className={styles.card}>
             <div className={styles.info}>
                 <div className={styles.textblock}>
                     <h1 className={styles.name}>Murphy the dog</h1>
