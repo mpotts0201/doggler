d136c676f8f44cb0e693b24e818f9fb4d6ebc59c
Murphy Potts <murphy@heliumservices.com>
2021-06-28 14:16:43 -0400

    Removed older version of _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,9 @@
-import '../styles/globals.css'
-import Router from 'lib/core/Router'
-import {withApp} from 'lib/core/wrapper'
-
-// function MyApp({ Component, pageProps, dispatch }) {
-//   return <Component {...pageProps} dispatch={dispatch} />
-// }
+import "../styles/globals.css";
+import Router from "lib/core/Router";
+import {withApp} from "lib/core/wrapper";
 
 function MyApp(props) {
-  return <Router {...props} />;
+    return <Router {...props} />;
 }
 
-export default withApp(MyApp)
+export default withApp(MyApp);
